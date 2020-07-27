ea6d3177d731f85cdfa8e2770e87fbb1641328ef
Ryan Bloor <ryanbloor@me.com>
2020-07-27 06:01:41 +0100

    see previous commit

--- src/views/Quiz/QuizResults.vue
+++ src/views/Quiz/QuizResults.vue
@@ -21,7 +21,7 @@
                   <v-icon color="green">mdi-check</v-icon>
                 </v-list-item-icon>
                 <v-list-item-content>
-                  <v-list-item-title class="green--text" v-text="category"></v-list-item-title>
+                  <v-list-item-title class="green--text" v-text="group.category.name"></v-list-item-title>
                 </v-list-item-content>
                 <v-list-item-content>
                   <v-list-item-title
