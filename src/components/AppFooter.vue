dd22e5f7afa526366a2fdd89b6dea492fdfd7b62
Ryan Bloor <ryanbloor@me.com>
2020-08-26 09:53:00 +0100

    remove footer link

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,7 +5,7 @@
         <div class="white--text text-center">
           Made with
           <v-icon class="red--text">mdi-heart</v-icon>by
-          <a class="white--text" href="https://ryanbloor.co.uk" target="_blank">Ryan Bloor</a>
+          <a class="white--text" href target="_blank">Ryan Bloor</a>
         </div>
       </v-flex>
     </v-layout>
